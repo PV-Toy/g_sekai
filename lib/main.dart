4cfe9c71e46e7d2b762e2874f3a9f37705fbceec
eve <eve@gna.company>
2023-05-25 19:55:45 +0900

    add: cloud_firestore package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:g_sekai/firebase_options.dart';
 import 'package:g_sekai/managers/route_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
