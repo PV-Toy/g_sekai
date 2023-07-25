c5d82e5e239e5ed83989aaf0432a121a3b2a1ec5
eve <eve@gna.company>
2023-07-25 19:48:58 +0900

    fix: bottom nav

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -77,10 +77,10 @@ class _MyHomePageState extends State<MyHomePage> {
               width: 40,
               height: 40,
             ),
-            SizedBox(
+            const SizedBox(
               width: 5,
             ),
-            Text("G.SEKAI"),
+            const Text("G.SEKAI"),
           ],
         ),
       ),
@@ -90,48 +90,69 @@ class _MyHomePageState extends State<MyHomePage> {
             topRight: Radius.circular(20.0),
           ),
           child: Container(
-              padding: const EdgeInsets.fromLTRB(25, 22, 38, 18),
-              height: 90,
-              color: const Color.fromRGBO(39, 11, 14, 0.9),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 57,
-                    height: 57,
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFF83F56),
-                          Color(0xFFA11538),
-                        ],
-                        stops: [0.0, 0.7136],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        transform:
-                            GradientRotation(3.14159), // 180 degrees in radians
-                      ),
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xFFF87D9F),
-                          offset: Offset(0, 4),
-                          blurRadius: 13.0,
-                          spreadRadius: -6.0,
-                        ),
-                      ],
+              padding: const EdgeInsets.fromLTRB(70, 22, 70, 25),
+              height: 80,
+              color: const Color.fromRGBO(254, 66, 86, 20),
+              child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                      size: 28,
                     ),
-                    padding: const EdgeInsets.all(7),
-                    child: Row(),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Icon(Icons.home),
-                        Icon(Icons.person_2_outlined),
-                      ]),
-                ],
-              ))),
+                    Icon(
+                      Icons.home,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    Icon(
+                      Icons.person_2_outlined,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                  ])
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Container(
+              //       width: 57,
+              //       height: 57,
+              //       decoration: const BoxDecoration(
+              //         gradient: LinearGradient(
+              //           colors: [
+              //             Color(0xFFF83F56),
+              //             Color(0xFFA11538),
+              //           ],
+              //           stops: [0.0, 0.7136],
+              //           begin: Alignment.topCenter,
+              //           end: Alignment.bottomCenter,
+              //           transform:
+              //               GradientRotation(3.14159), // 180 degrees in radians
+              //         ),
+              //         shape: BoxShape.circle,
+              //         boxShadow: [
+              //           BoxShadow(
+              //             color: Color(0xFFF87D9F),
+              //             offset: Offset(0, 4),
+              //             blurRadius: 13.0,
+              //             spreadRadius: -6.0,
+              //           ),
+              //         ],
+              //       ),
+              //       padding: const EdgeInsets.all(7),
+              //       child: Row(),
+              //     ),
+              //     const Row(
+              //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //         children: [
+              //           Icon(Icons.favorite),
+              //           Icon(Icons.home),
+              //           Icon(Icons.person_2_outlined),
+              //         ]),
+              //   ],
+              // )
+              )),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
@@ -153,7 +174,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text("VALORANT"),
+                        Text("VALORANT"),
                         Row(
                           children: [
                             Icon(Icons.favorite_outline),
@@ -178,6 +199,10 @@ class _MyHomePageState extends State<MyHomePage> {
                               // child: MediaWidget(imageUrl: imageUrlList[index])
                             )),
                   ),
+                  YoutubePlayer(
+                    controller: _controller,
+                    aspectRatio: 16 / 9,
+                  )
                 ],
               ),
             ],
